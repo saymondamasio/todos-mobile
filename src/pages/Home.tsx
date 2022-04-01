cbe18f28b879564ccf2f83d8f0c77c0ef61e9ad2
Saymon Damásio <saymon.damasio95@gmail.com>
2022-04-01 09:14:01 -0300

    feat: done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,14 +10,31 @@ export function Home() {
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task
+    setTasks(oldState => [...oldState, {
+      done: false,
+      title: newTaskTitle,
+      id: new Date().getTime()
+    }])
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({ ...task }))
+    const updatedTask = updatedTasks.find(task => task.id === id)
+    if(updatedTask) {
+      updatedTask.done = !updatedTask.done
+    }
+    
+    setTasks(updatedTasks)
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    const task = tasks.find(task => task.id)
+    if(task) {
+      task.done = !task.done
+      setTasks(oldState => oldState.filter(task => task.id !== id))
+    }
   }
 
   return (
